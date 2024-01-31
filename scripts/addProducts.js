f4180b5a1dc4a4845eb32f7660f93e15ab47580b
Emeline LD <emeline.ledu@meline-digital.fr>
2024-01-31 15:01:17 +0100

    get data testing

--- scripts/addProducts.js
+++ scripts/addProducts.js
@@ -1,14 +1,23 @@
 
+
+
 window.onload = (event) => {
 
+    let datas = [];
+    fetch('../utils/books.json')
+        .then((response) => response.json())
+        .then((json) => datas.push(json));
+
+    console.log(datas);
+    let dataStorage = JSON.stringify(datas[0]);
+    console.log(dataStorage);
     console.log("page is fully loaded");
+    localStorage.setItem("datas", dataStorage)
+    console.log(localStorage.getItem("datas"));
 
     document.getElementById('submit').addEventListener('submit', (e) => {
         e.preventDefault;
 
-        fetch('../utils/books.json')
-            .then((response) => response.json())
-            .then((json) => console.log(json));
 
         try {
             let formData = {
