1d236333b04b3067e00456a767ccfc0bf1d047c4
eselene <eviougeas@gmail.com>
2024-02-03 11:30:51 +0100

    intégration navBar

--- scripts/searchUser.js
+++ scripts/searchUser.js
@@ -1,8 +1,8 @@
 let usersArray = [
-    { nom: "Macron", prenom: "Dora", email: "dora@exampledora.com", telephone: "067625252", site_web: "www.ahotmail.fr", commentaire: "fqfdqdfqsdfqsdf qd fqsdf" },
-    { nom: "benway", prenom: "doe", email: "doe@exampledoe.com", telephone: "067625251", site_web: "www.bhotmail.fr", commentaire: "fqfdqdfqsdfqsdf qd fqsdf" },
-    { nom: "fi", prenom: "Jo", email: "johny@examplejohn.com", telephone: "0676252553", site_web: "www.chotmail.fr" },
-    { nom: "Le dû", prenom: "Anabelle", email: "anabelle@examplele.com", telephone: "067625254", site_web: "www.dhotmail.fr" }
+    { nom: "Macron", prenom: "Dora", email: "dora@exampledora.com", telephone: "067625252", site_web: "www.ahotmail.fr", commentaire: "fqfdqdfqsdfqsdf qd fqsdf", images:"../images/carla.jpg" },
+    { nom: "benway", prenom: "doe", email: "doe@exampledoe.com", telephone: "067625251", site_web: "www.bhotmail.fr", commentaire: "fqfdqdfqsdfqsdf qd fqsdf", images:"../images/david.jpg" },
+    { nom: "pi", prenom: "po", email: "johny@examplejohn.com", telephone: "0676252553", site_web: "www.chotmail.fr"},
+    { nom: "Le dû", prenom: "belle", email: "anabelle@examplele.com", telephone: "067625254", site_web: "www.dhotmail.fr", images: "../images/carla.jpg"  }
 ];
 const msgeNonTrouve = "Utilisateur non trouvé";
 // Get the form element by its id
@@ -15,7 +15,7 @@ form.addEventListener('submit', (e) => {
         let nomRecherche = document.getElementById('nomRecherche').value;
         let prenomRecherche = document.getElementById('prenomRecherche').value;
 
-        let user = usersArray.find(u => u.nom == nomRecherche && u.prenom.toLowerCase() == prenomRecherche.toLowerCase());
+        let user = usersArray.find(u => u.nom.toLowerCase() == nomRecherche.toLowerCase() && u.prenom.toLowerCase() == prenomRecherche.toLowerCase());
 
         if (user) {
             document.getElementById('nom').value = user.nom;
@@ -25,6 +25,12 @@ form.addEventListener('submit', (e) => {
             document.getElementById('site_web').value = user.site_web;
             if (user.commentaire != undefined) {
                 document.getElementById('commentaire').value = user.commentaire;
+            }
+            if (user.images != undefined) {
+                document.getElementById('myImage').src  = user.images;
+            }            
+            else {
+                document.getElementById('myImage').src  = "../images/nophoto.jpg";
             }
             document.getElementById('message').textContent = "";
         } else {
