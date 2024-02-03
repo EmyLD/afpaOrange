d02f4c31d42a4a977ef1ae5328142bf1b7556bfc
eselene <eviougeas@gmail.com>
2024-02-03 09:29:37 +0100

    MAJ présentation fonctionalité searchuser

--- scripts/searchUser.js
+++ scripts/searchUser.js
@@ -1,49 +1,34 @@
-// alert("cc");
-var usersArray = [
-    { nom: "Macron", prenom: "Dora", email: "dora@exampledora.com", telephone: "067625252", site_web: "www.ahotmail.fr" },
-    { nom: "benway", prenom: "doe", email: "doe@exampledoe.com", telephone: "067625251", site_web: "www.bhotmail.fr" }
-    ,{ nom: "Philippe", prenom: "Johnny", email: "johny@examplejohn.com", telephone: "0676252553", site_web: "www.chotmail.fr" }
-    ,{ nom: "Le dû", prenom: "Anabelle", email: "anabelle@examplele.com", telephone: "067625254", site_web: "www.dhotmail.fr" }
+let usersArray = [
+    { nom: "Macron", prenom: "Dora", email: "dora@exampledora.com", telephone: "067625252", site_web: "www.ahotmail.fr", commentaire: "fqfdqdfqsdfqsdf qd fqsdf" },
+    { nom: "benway", prenom: "doe", email: "doe@exampledoe.com", telephone: "067625251", site_web: "www.bhotmail.fr", commentaire: "fqfdqdfqsdfqsdf qd fqsdf" },
+    { nom: "fi", prenom: "Jo", email: "johny@examplejohn.com", telephone: "0676252553", site_web: "www.chotmail.fr" },
+    { nom: "Le dû", prenom: "Anabelle", email: "anabelle@examplele.com", telephone: "067625254", site_web: "www.dhotmail.fr" }
 ];
+const msgeNonTrouve = "Utilisateur non trouvé";
 // Get the form element by its id
 let form = document.getElementById("searchForm");
 // Add an event listener for the click event
-
-// form.addEventListener('submit', (e) =>{
-//     e.preventDefault();
-//     document.getElementById('donate').style.display = 'none';
-//     document.getElementById('topMessage').style.display = 'none';
-// });
-
 form.addEventListener('submit', (e) => {
-    // alert("form.addEventListener(submit, (e) =>");
     //pour qu'il se reinitialise pas
     e.preventDefault();
     try {
         let nomRecherche = document.getElementById('nomRecherche').value;
         let prenomRecherche = document.getElementById('prenomRecherche').value;
-        // alert("2F cc");
 
-        debugger;
         let user = usersArray.find(u => u.nom == nomRecherche && u.prenom.toLowerCase() == prenomRecherche.toLowerCase());
-        // let user = usersArray.find(u => u.nom == nomRecherche && u.prenom == prenomRecherche );
 
-        // Get the input elements by their names
         if (user) {
-            // Set the values of the input elements to the user data
-
-            // alert(user.prenom);
-
             document.getElementById('nom').value = user.nom;
             document.getElementById('prenom').value = user.prenom;
             document.getElementById('e-mail').value = user.email;
             document.getElementById('telephone').value = user.telephone;
             document.getElementById('site_web').value = user.site_web;
-            // alert(user.nom);
-            // Show the form
-            // document.getElementById('userForm').style.display = 'block';
+            if (user.commentaire != undefined) {
+                document.getElementById('commentaire').value = user.commentaire;
+            }
+            document.getElementById('message').textContent = "";
         } else {
-            alert('User not found!');
+            document.getElementById('message').textContent = msgeNonTrouve;
         }
     } catch (error) {
         console.log(error);
